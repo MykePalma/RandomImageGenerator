b2967926c57b1b5276aa2de1d8177b2f44ee5bda
myke palma <palmamyke@gmail.com>
2024-09-24 18:28:41 +0100

    Add project files.

---
+++ ColorMap.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+
+namespace ContentGenerator;
+
+public class ColorMap
+{
+    public Random _random { get; set; }
+    private List<Color> Colors { get; set; }
+
+    public ColorMap()
+    {
+        _random = new Random();
+        Colors = [];
+
+        for (int i = 0; i < 14; i++)
+        {
+            Colors.Add(GenerateRandomColor());
+        }
+    }
+
+    public Color GetColor(float v)
+    {
+        if (v < -0.35)
+        {
+            return Colors[0];
+        }
+        else if (v < -0.25)
+        {
+            return Colors[1];
+        }
+        else if (v < -0.15)
+        {
+            return Colors[2];
+        }
+        else if (v < -0.1)
+        {
+            return Colors[3];
+        }
+        else if (v < 0.0)
+        {
+            return Colors[4];
+        }
+        else if (v < 0.1)
+        {
+            return Colors[5];
+        }
+        else if (v < 0.2)
+        {
+            return Colors[6];
+        }
+        else if (v < 0.5)
+        {
+            return Colors[7];
+        }
+        else if (v < 0.7)
+        {
+            return Colors[8];
+        }
+        else if (v < 0.8)
+        {
+            return Colors[9];
+        }
+        else if (v < 0.85)
+        {
+            return Colors[10];
+        }
+        else if (v < 0.90)
+        {
+            return Colors[11];
+        }
+        else if (v <= 0.9995)
+        {
+            return Colors[12];
+        }
+        else if (v <= 1.0 - 0.000001)
+        {
+            return Colors[13];
+        }
+        else
+        {
+            return Colors[0];
+        }
+    }
+
+    private Color GenerateRandomColor()
+    {
+        return Color.FromArgb(255, _random.Next(0, 255), _random.Next(0, 255), _random.Next(0, 255));
+    }
+}
---
+++ NoiseImageGenerator.cs
@@ -0,0 +1,93 @@
+﻿using Generation.Noise;
+using System.Drawing;
+using static Generation.Noise.FastNoiseLite;
+
+namespace ContentGenerator;
+
+public class NoiseImageGenerator
+{
+    private readonly int _size;
+    private readonly NoiseType _noiseType;
+    private readonly FractalType _fractalType;
+    private readonly float _frequency;
+    private readonly int _octaves;
+
+    public Random RandomNumber { get; set; }
+    public ColorMap ColorMap { get; set; }
+
+    public NoiseImageGenerator(int? size)
+    {
+        RandomNumber = new Random();
+        ColorMap = new ColorMap();
+        _size = size ?? 1024;
+        _noiseType = NoiseType.OpenSimplex2;
+        _fractalType = FractalType.FBm;
+        _frequency = 0.005f;
+        _octaves = 8;
+    }
+
+    public NoiseImageGenerator(int size, NoiseType noiseType, FractalType fractalType, float frequency, int octaves)
+    {
+        RandomNumber = new Random();
+        ColorMap = new ColorMap();
+        FastNoiseLite noise = new(RandomNumber.Next(1, 20000));
+        _size = size;
+        _noiseType = noiseType;
+        _fractalType = fractalType;
+        _frequency = frequency;
+        _octaves = octaves;
+    }
+
+    public void Generate()
+    {
+        FastNoiseLite noise = new(RandomNumber.Next(1, 20000));
+
+        noise.SetNoiseType(_noiseType);
+        noise.SetFractalType(_fractalType);
+        noise.SetFrequency(_frequency);
+        noise.SetFractalOctaves(_octaves);
+
+        int sizeX = _size;
+        int sizeY = _size;
+
+        Bitmap bmp = new(sizeX, sizeY);
+
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int y = 0; y < sizeY; y++)
+            {
+                float calc1 = noise.GetNoise(x, y);
+                bmp.SetPixel(x, y, color: ColorMap.GetColor(calc1));
+            }
+        }
+
+        SaveBitmapToFile(bmp);
+    }
+
+    private void SaveBitmapToFile(Bitmap bmp)
+    {
+        using (var stream = new MemoryStream())
+        {
+            bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] byteArray = stream.ToArray();
+
+            string folderPath = Path.Combine(Environment.CurrentDirectory, "Generated\\");
+
+            if (!File.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = folderPath + string.Join(".", _size.ToString(), _noiseType.ToString(), _fractalType.ToString(), Guid.NewGuid() + ".jpg");
+
+            var newfile = File.OpenWrite(filePath);
+
+            newfile.Write(byteArray, 0, byteArray.Length);
+        }
+    }
+
+    private Color GenerateRandomColor()
+    {
+        return Color.FromArgb(255, RandomNumber.Next(0, 255), RandomNumber.Next(0, 255), RandomNumber.Next(0, 255));
+    }
+}
